03c6b11a05019c7f74e550bbf79092e04411c27b
jojo--1 <1468608544@qq.com>
2024-11-22 23:07:57 +0800

    C：实现一个函数，可以左旋字符串中的 k 个字符

--- 凯伦/凯伦/test1.cpp
+++ 凯伦/凯伦/test1.cpp
@@ -1,38 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-//  char类型指针 str，指向要反转的字符串的起始位置，sz 表示字符串的长度
-void reverse_string(char* str, int sz)
+// 可以设计循环使其可以旋 1 次，然后让其执行 n 次
+
+#include<string.h>  // 包含标准字符串处理库，以便使用 strlen函数计算字符串长度
+void leftRound(char arr[], int len, int time)
 {
-	// 指针left 指向字符串的起始位置
-	char* left = str;
+	int i = 0;
+	int j = 0;
 
-	// 指针right，指向字符串的最后一个字符的位置(即 str + sz - 1,减去 1 是因为数组索引是从 0 开始的)
-	char* right = str + sz - 1;
+	// 取 time 和字符串长度 len 的模，以避免 time 大于 len 的情况
+	// 如：长度为 6 的情况下，旋转 7、13、19...次相当于 1 次，8、14、20...次相当于 2 次
+	time %= len;
 
-	// 对字符串的中间部分进行交换操作，直到两个指针相遇
-	while (left < right)
+	// 执行 time 次的单次平移
+	// 即循环 time 次，每次循环将字符串左旋 1 个字符
+	for (i = 0; i < time; i++)
 	{
-		// 交换两个指针 left 和 right 所指向的字符
-		char tmp = *right;
-		*right = *left;
-		*left = tmp;
+		char tmp = arr[0];
+		// 单次平移
+		for (j = 0; j < len - 1; j++)
+		{
+			arr[j] = arr[j + 1];
+		}
+		arr[j] = tmp;  // 也可以写成这样 arr[len - 1] = tmp;
 
-		// 指针left 向后移动一个字符
-		left++;
-		// 指针right 向前移动一个字符
-		right--;
 	}
 }
-
-#include<string.h>
 int main()
 {
-
 	char arr[] = "abcdef";
-	int sz = strlen(arr);     // 计算字符串 arr 的长度
-	reverse_string(arr, sz);
+	int time = 0;   // 表示左旋次数
+	scanf("%d", &time);
+	int len = strlen(arr);   // 使用 strlen函数计算字符串 arr 的长度
+	leftRound(arr, len, time);
 	printf("%s", arr);
-
 	return 0;
 }
