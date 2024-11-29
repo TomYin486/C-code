f67e69a18e665196542e62549ba6031ec3551d9e
jojo--1 <1468608544@qq.com>
2024-11-29 22:17:28 +0800

    C：给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置（使用二分查找）

--- 凯伦/凯伦/test1.cpp
+++ 凯伦/凯伦/test1.cpp
@@ -1,42 +1,49 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<string.h>
 
-// 函数 reverse_part，接受字符指针src（指向字符串的起始位置）、整数start（逆序开始的位置）和整数end（逆序结束的位置）
-// 将字符串从 start 到 end 这一段逆序
-void reverse_part(char* src, int start, int end)
+// 接受 3 个参数：整数数组 nums 的指针，数组的大小 numsSize，以及要查找的目标值 target
+int searchInsert(int* nums, int numsSize, int target)
 {
-	int i = 0;
-	int j = 0;
+    // left 和 right，分是数组的起始位置和结束位置的下标
+    int left = 0;
+    int right = numsSize - 1;
 
-	for (i = start, j = end; i < j; i++, j--)
-	{
-		char tmp = src[i];
-		src[i] = src[j];
-		src[j] = tmp;
+    // 用于表示 target 应该插入的位置，如果 target 比数组中的所有元素都大，ans 将保持为 numsSize
+    int ans = numsSize;
+    while (left <= right)
+    {
+        int mid = (right - left) / 2 + left;
 
-	}
+        // 如果 target 小于或等于中间下标处的值，更新 ans 为 mid，并调整 right 为 mid - 1
+        // 这样下一次循环只会在数组的左半部分查找
+        if (target <= nums[mid])
+        {
+            ans = mid;
+            right = mid - 1;
 
+        }
+        // 如果 target 大于中间下标处的值，调整 left 为 mid + 1
+        // 这样下一次循环只会在数组的右半部分查找
+        else
+        {
+            left = mid + 1;
+        }
+    }
+    // 表示 target 应该插入的位置
+    return ans;
 }
-
-// 例如：左旋 3 次字符串 ABCDEFG，结果为 DEFGABC
-// 先将要左旋的前 3 个字符串逆序（CBADEFG），然后将后半段也逆序（CBAGFED），最后整体逆序（DEFGABC）即可
-// 这样只需要做数值交换即可，可以写个上述函数来完成局部逆序
 
 
 int main()
 {
-	int time = 0;
-	char arr[] = "ABCD";
-	scanf("%d", &time);
+    int nums[] = { -1,0,1,2,3,4 };
+    int target = 0;
+    scanf("%d", &target);
+    int numsSize = sizeof(nums) / sizeof(nums[0]);   // 计算数组 nums 的大小
+    int ret = searchInsert(nums, numsSize, target);
 
-	int len = strlen(arr);
-	int pos = time % len;
+    // 打印目标值(target)在数组中的下标，或者它应该被插入的位置
+    printf("%d\n", ret);
 
-	reverse_part(arr, 0, pos - 1);   //逆序前段,将字符串 arr 从下标 0 到 pos - 1 的子串逆序
-	reverse_part(arr, pos, len - 1); //逆序后段，将字符串 arr 从下标 pos 到 len - 1 的子串逆序
-	reverse_part(arr, 0, len - 1);   //整体逆序
-	printf("%s", arr);
-
-	return 0;
+    return 0;
 }
