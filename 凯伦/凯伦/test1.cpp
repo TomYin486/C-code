476088b40f52940bc0b3b49920e26a674af8cee0
jojo--1 <1468608544@qq.com>
2024-12-14 22:42:48 +0800

    C：给定一个整数数组 nums 和一个整数目标值 target，在该数组中找出和为目标值 target 的两个整数（找出一对就行），并返回它们的数组下标

--- 凯伦/凯伦/test1.cpp
+++ 凯伦/凯伦/test1.cpp
@@ -1,52 +1,61 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<stdlib.h> // 包含 stdlib.h 头文件，可使用 malloc 和 free 等动态内存分配函数
 
-int waysToStep(int n)
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
-	// count 用于在循环中计数，从 3 开始，因为至少需要走 3 步才能开始计算
-	int count = 3;
+    int i = 0;
+    for (i = 0; i < numsSize; ++i)
+    {
+        int j = 0;
+        // 从当前外层循环元素的下一个元素开始，确保不会使用相同的元素两次
+        for (j = i + 1; j < numsSize; j++)
+        {
+            // 检查当前外层循环的元素 nums[i] 和内层循环的元素 nums[j] 的和是否等于目标值 target
+            if (nums[i] + nums[j] == target)
+            {
+                // 如果找到了这样的两个数，使用 malloc 分配一个大小为 2 的整数数组的内存空间，用于存储两个下标
+                int* ret = (int*)malloc(sizeof(int) * 2);
+                if (ret == NULL)
+                {
+                    // 如果内存分配失败
+                    return NULL;
+                }
+                // 将找到的两个数的下标赋值给 ret 数组的两个元素
+                ret[0] = i;
+                ret[1] = j;
 
-	// 存储到达每一级台阶的不同方式的数量
-	// arr[0]，arr[1]，arr[2] 分别表示到达前一阶、前两阶和前三阶台阶的不同方式的数量
-	int arr[4] = { 0 };
-	arr[0] = 1;
-	arr[1] = 2;
-	arr[2] = 4;
+                // 将 returnSize 指向的值设置为 2，表示返回的数组中有两个元素
+                *returnSize = 2;
 
-	if (n <= 2)
-	{
-		return n;
-	}
-	else if (n == 3)
-	{
-		return 4;
-	}
+                // 返回包含两个下标的数组
+                return ret;
+            }
+        }
+    }
+    *returnSize = 0;
 
-	// 直到 count 等于 n，即到达最后一阶台阶
-	while (count < n)
-	{
-		// 将到达前 3 个台阶的方式数量相加并取模，以避免结果过大
-		arr[3] = (arr[0] + arr[1]) % 1000000007 + arr[2];  // 计算到达第 count 阶的方式数量
-		arr[3] %= 1000000007;     // 取模以避免溢出
-
-		/// 将 arr[0], arr[1], arr[2] 的值向前移动
-		arr[0] = arr[1];
-		arr[1] = arr[2];
-		arr[2] = arr[3];
-
-		// 移动到下一阶
-		count++;
-	}
-	// 返回数组中最后 1 个元素的值，即到达第 n 阶台阶的不同方式的数量
-	return arr[3];
+    // 返回 NULL，表示没有找到符合条件的两个数
+    return NULL;
 }
 
 int main()
 {
-	int n = 0;
-	scanf("%d", &n);
-	int ret = waysToStep(n);
-	printf("%d\n", ret);
+    int nums[] = { 1,2,3,4,5 };
+    int target = 0;
+    int returnSize = 0;
+    scanf("%d", &target);
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
 
-	return 0;
+    int* ret = twoSum(nums, numsSize, target, &returnSize);
+    if (ret != NULL)
+    {
+        printf("找到了，两个下标是:[%d,%d]\n", ret[0], ret[1]);
+        free(ret);  // 释放ret指向的内存
+    }
+    else
+    {
+        printf("没有找到符合条件的两个数，或空间开辟失败\n");
+    }
+    return 0;
 }
