736bf9fd09fd01a985474fd478765602433d0df6
jojo--1 <1468608544@qq.com>
2024-12-15 22:01:35 +0800

    C：给定 n 个整数 a1，a2，.......... an，求它们两两相乘再相加的和 （如 3 个整数：1，2，3，则计算 1*2 + 1*3 + 2*3）

--- 凯伦/凯伦/test1.cpp
+++ 凯伦/凯伦/test1.cpp
@@ -1,61 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<stdlib.h> // 包含 stdlib.h 头文件，可使用 malloc 和 free 等动态内存分配函数
 
-int* twoSum(int* nums, int numsSize, int target, int* returnSize)
-{
-    int i = 0;
-    for (i = 0; i < numsSize; ++i)
-    {
-        int j = 0;
-        // 从当前外层循环元素的下一个元素开始，确保不会使用相同的元素两次
-        for (j = i + 1; j < numsSize; j++)
-        {
-            // 检查当前外层循环的元素 nums[i] 和内层循环的元素 nums[j] 的和是否等于目标值 target
-            if (nums[i] + nums[j] == target)
-            {
-                // 如果找到了这样的两个数，使用 malloc 分配一个大小为 2 的整数数组的内存空间，用于存储两个下标
-                int* ret = (int*)malloc(sizeof(int) * 2);
-                if (ret == NULL)
-                {
-                    // 如果内存分配失败
-                    return NULL;
-                }
-                // 将找到的两个数的下标赋值给 ret 数组的两个元素
-                ret[0] = i;
-                ret[1] = j;
+// 定义数组 a 和 sum 的最大大小，以确保能够存储足够多的整数
+#define MAXN 200005
 
-                // 将 returnSize 指向的值设置为 2，表示返回的数组中有两个元素
-                *returnSize = 2;
-
-                // 返回包含两个下标的数组
-                return ret;
-            }
-        }
-    }
-    *returnSize = 0;
-
-    // 返回 NULL，表示没有找到符合条件的两个数
-    return NULL;
-}
+int n = 0;
+int a[MAXN] = { 0 };   // 存储输入的整数
+long long sum[MAXN] = { 0 };   // 用来计算前缀和
+long long ans = 0;     // 用来存储最终的两两相乘的和
 
 int main()
 {
-    int nums[] = { 1,2,3,4,5 };
-    int target = 0;
-    int returnSize = 0;
-    scanf("%d", &target);
-    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    scanf("%d", &n);
+    int i = 0;
 
-    int* ret = twoSum(nums, numsSize, target, &returnSize);
-    if (ret != NULL)
+    // 读取输入的每个整数并存储在数组 a 的相应位置
+    for (i = 1; i <= n; i++)
     {
-        printf("找到了，两个下标是:[%d,%d]\n", ret[0], ret[1]);
-        free(ret);  // 释放ret指向的内存
+        scanf("%d", &a[i]);
+        // 计算前缀和 sum[i]，它是 sum[i-1]（前一个元素的前缀和）加上当前元素 a[i]
+        sum[i] = sum[i - 1] + a[i];
     }
-    else
+
+    // 计算两两相乘的和
+    for (i = 1; i <= n - 1; i++)
     {
-        printf("没有找到符合条件的两个数，或空间开辟失败\n");
+        // 计算 a[i] 与除了它以外的所有其他数的乘积之和，并将结果累加到 ans 中
+        // sum[n] - sum[i] 表示除了 a[i] 以外的所有数的和
+        ans += a[i] * (sum[n] - sum[i]);
     }
+    printf("%lld\n", ans);
     return 0;
 }
