1899671e22eee4b668a6cc515e9c7de23f20652f
jojo--1 <1468608544@qq.com>
2024-12-18 19:25:53 +0800

    C：打印一个 n （n 为大于等于 2 的整数）进制的乘法表

--- 凯伦/凯伦/test1.cpp
+++ 凯伦/凯伦/test1.cpp
@@ -1,34 +1,69 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-// 定义数组 a 和 sum 的最大大小，以确保能够存储足够多的整数
-#define MAXN 200005
+// 将十进制数转换为 n 进制数
+void func1(int num, int base)
+{
+    // 假设数组足够大，可以存储转换后的数字
+    char result[100];
+    int index = 0;
 
-int n = 0;
-int a[MAXN] = { 0 };   // 存储输入的整数
-long long sum[MAXN] = { 0 };   // 用来计算前缀和
-long long ans = 0;     // 用来存储最终的两两相乘的和
+    // 如果数字为 0，直接返回
+    if (num == 0)
+    {
+        printf("0");
+        return;
+    }
+
+
+    while (num > 0)
+    {
+        // 取余数
+        int remainder = num % base;
+
+        if (remainder < 10)
+        {
+            // 转换为字符并存储
+            result[index++] = '0' + remainder;
+        }
+        else
+        {
+            // 使用大写字母表示 10 以上的数字
+            result[index++] = 'A' + remainder - 10;
+        }
+        // 更新 num 为 num/base 的整数部分
+        num /= base;
+    }
+
+    int i = 0;
+    // 结果是反向存储的，要反向打印
+    for (i = index - 1; i >= 0; i--)
+    {
+        printf("%c", result[i]);
+    }
+}
 
 int main()
 {
-    scanf("%d", &n);
     int i = 0;
+    int j = 0;
+    int n = 0;
+    scanf("%d", &n);
 
-    // 读取输入的每个整数并存储在数组 a 的相应位置
-    for (i = 1; i <= n; i++)
+    for (i = 1; i < n; i++)
     {
-        scanf("%d", &a[i]);
-        // 计算前缀和 sum[i]，它是 sum[i-1]（前一个元素的前缀和）加上当前元素 a[i]
-        sum[i] = sum[i - 1] + a[i];
+        for (j = 1; j <= i; j++)
+        {
+            int product = i * j;
+            func1(i, n);   // 输出第一个乘数
+            printf("*");
+            func1(j, n);   // 输出第二个乘数
+            printf("=");
+            func1(product, n); // 输出乘积
+            printf(" ");
+        }
+        printf("\n");
     }
 
-    // 计算两两相乘的和
-    for (i = 1; i <= n - 1; i++)
-    {
-        // 计算 a[i] 与除了它以外的所有其他数的乘积之和，并将结果累加到 ans 中
-        // sum[n] - sum[i] 表示除了 a[i] 以外的所有数的和
-        ans += a[i] * (sum[n] - sum[i]);
-    }
-    printf("%lld\n", ans);
     return 0;
 }
