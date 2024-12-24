3454bdde8181940735f66004df6e20fb34fc7713
jojo--1 <1468608544@qq.com>
2024-12-24 20:40:42 +0800

    C：计算从 1 到给定的正整数 n 之间，满足奇数位上数字为奇数、偶数位上数字为偶数的“好 数”的总数

--- 凯伦/凯伦/test1.cpp
+++ 凯伦/凯伦/test1.cpp
@@ -1,84 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 
-// t 表示测试数据的组数，n 表示每组数据中飞机的数量，bk 为一个布尔数组，用于标记飞机是否已经被安排降落
-int t = 0;
-int n = 0;
-int bk[15] = { 0 };
-
-// 结构体 plane 包含 t（到达时间）、d（盘旋时间）和 l（降落时间）
-struct plane {
-    int t;
-    int d;
-    int l;
-
-} a[15];  // 数组 a 中的元素为该结构体类型，用于存储每架飞机的信息
-
-// dep 表示当前处理的飞机编号（从 1 开始），tim 表示当前的时间
-int dfs(int dep, int tim)
+int func1(int x)
 {
-    int i = 0;
+	// 用于表示当前检查的位数（奇数位或偶数位）
+	int i = 1;
+	while (x)
+	{
+		// 如果 i 为奇数位
+		if (i % 2 == 1)
+		{
+			// 如果 x 的当前最低位是偶数，则 x 不是好数，函数返回 0
+			if ((x % 10) % 2 == 0)
+			{
+				return 0;
+			}
+		}
+		// 如果 i 为偶数位
+		else
+		{
+			// 如果 x 的当前最低位是奇数，则 x 不是好数，函数返回 0
+			if ((x % 10) % 2 == 1)
+			{
+				return 0;
+			}
+		}
 
-    // 如果 dep 大于飞机总数 n，说明所有飞机都已处理完毕，则返回 1 表示成功
-    if (dep > n)
-    {
-        return 1;
-    }
-
-    // 遍历所有飞机
-    for (i = 1; i <= n; i++)
-    {
-        // 如果飞机 i 已经被标记为处理过（bk[i] 为真）或者飞机 i 的 t（到达时间）加 d（上盘旋时间）小于当前时间 tim（即飞机已经无法降落），则跳过这架飞机
-        if (bk[i] || a[i].t + a[i].d < tim) // 降落条件
-            continue;
-        bk[i] = 1;
-
-        // 计算飞机 i 降落结束后的时间 next_tim，取 tim 和飞机 i 的到达时间 a[i].t 中的较大值
-        int next_tim = (tim > a[i].t) ? tim : a[i].t;
-
-        // 更新 next_tim（为飞机 i 降落结束后的时间）
-        next_tim += a[i].l;
-
-        // 递归调用 dfs 函数处理下一架飞机，如果成功，则将当前飞机标记为未处理，并返回 1
-        if (dfs(dep + 1, next_tim))
-        {
-            bk[i] = 0;
-            return 1; // 有一个成功就返回1
-        }
-        // 如果当前飞机无法降落，则将其标记为未处理，并继续尝试其他飞机
-        bk[i] = 0;
-    }
-    // 如果所有飞机都尝试过后都无法降落，则返回 0 表示失败
-    return 0;
+		x /= 10;   // 将 x 除以 10，去掉当前最低位，检查下一位
+		i++;       // 计数器 i 加 1
+	}
+	return 1;
 }
 
 int main()
 {
-    scanf("%d", &t);    // t 为测试数据的组数 
-    while (t)
-    {
-        scanf("%d", &n);  // n 为飞机的数量
+	int i = 0;
+	int n = 0;
+	int count = 0;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		// 如果 func1 函数返回 1，说明 i 是一个好数
+		if (func1(i) == 1)
+		{
+			count++;
+		}
+	}
+	printf("%d\n", count);
 
-        int i = 0;
-        // 输入每架飞机的 t（到达时间）、d（盘旋时间）和 l（降落时间）
-        for (i = 1; i <= n; i++)
-        {
-            scanf("%d %d %d", &a[i].t, &a[i].d, &a[i].l);
-        }
-
-        // 清空 bk 数组，为新的一组数据做准备
-        memset(bk, 0, sizeof(bk));
-
-        if (dfs(1, 0))
-        {
-            printf("YES\n");
-        }
-        else
-        {
-            printf("NO\n");
-        }
-        t--;
-    }
-    return 0;
+	return 0;
 }
