d5dbec152c333819e5a934dfa0ff5a81c8bc34b7
jojo--1 <1468608544@qq.com>
2024-12-30 20:51:57 +0800

    C：给定一个长度为 n (n > 2)的数组 A = (a0,a1,...,a(n-1)，0,1...,n-1 为其下标)，判 断它是否是一个斐波那契数组。如果不是，计算最少需要修改多少个元素才能使其成为一个斐波那契数组(a0 = a1 = 1 且 i >= 2 时，满足 ai = a(i-1) + a(i-2)，i，i-1，i-2 为 其下标)

--- 凯伦/凯伦/test1.cpp
+++ 凯伦/凯伦/test1.cpp
@@ -1,45 +1,71 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
-void swap_arr(int arr[], int sz)
-{
-	int left = 0;
-	int right = sz - 1;
-	int tmp = 0;
+#define MAXN 100000   // 表示数组的最大大小
+#define MAXA 1000000  // 表示 a[i] 的最大值
 
-	while (left < right)
-	{
-		// 从前往后，找一个偶数，找到后停止
-		while ((left < right) && (arr[left] % 2 == 1))
-		{
-			left++;
-		}
-
-		// 从后往前找，找一个奇数，找到后停止
-		while ((left < right) && (arr[right] % 2 == 0))
-		{
-			right--;
-		}
-
-		// 如果偶数和奇数都找到，交换这两个数据的位置
-		// 然后继续找，直到两个指针相遇
-		if (left < right)
-		{
-			tmp = arr[left];
-			arr[left] = arr[right];
-			arr[right] = tmp;
-		}
-	}
-}
+long long f[MAXN];    // 数组 f 用于存入斐波那契数列
+long long a[MAXN];    // 数组 a 用于存入输入的整数序列
+int bot[MAXA];        // 数组 bot 用于统计 a[i] / f[i] 的出现次数
 
 int main()
 {
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int i = 0;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	swap_arr(arr, sz);
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", arr[i]);
-	}
+    int n = 0;           // 表示输入的整数序列的长度
+    scanf("%d", &n);
+
+    long long az = -1;   // az 用于记录 a[i] 的最大值
+    int i = 0;
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%lld", &a[i]);
+        if (a[i] > az)
+        {
+            az = a[i];  // 更新 az
+        }
+    }
+
+    // 初始化斐波那契数列
+    f[1] = f[2] = 1;
+    int m = 0;       // m 表示第一个 <= 1e6 的 f[i] 的位置
+    for (i = 3; i <= n; i++)
+    {
+        f[i] = f[i - 1] + f[i - 2]; // 计算斐波那契数列
+        if (f[i] > 1000000)
+        {
+            // 如果 f[i] 超过 1e6
+            m = i - 1;              // 记录 m 的位置
+            break;
+        }
+    }
+
+    // 如果 m 没有改变，说明 f[n] <= 1e6，直接赋值为 n
+    if (m == 0)
+    {
+        m = n;
+    }
+
+    // 统计 a[i] / f[i] 的出现次数
+    for (i = 1; i <= m; i++)
+    {
+        if (a[i] % f[i] == 0)
+        {
+            // 如果 a[i] 能被 f[i] 整除
+            bot[a[i] / f[i]]++;     // 统计 a[i] / f[i] 的出现次数
+        }
+    }
+
+    // 找到 bot 数组中的最大值
+    int maxc = -1;
+    for (i = 1; i <= az; i++)
+    {
+        if (bot[i] > maxc)
+        {
+            maxc = bot[i];   // 更新 maxc
+        }
+    }
+
+    printf("%d\n", n - maxc);
+
+    return 0;
 }
