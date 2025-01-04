3444254efa7a1cd6968f0c206e8b8edc18151586
jojo--1 <1468608544@qq.com>
2025-01-04 21:08:23 +0800

    C：（使用 dp）给定两个字符串 S 和 T，计算通过最少的修改次数使 S 包含 T 作为其子序 列。子序列可理解为可以从 S 中按顺序选取字符来组成 T，但不要求这些字符在 S 中是连续 的。

--- 凯伦/凯伦/test1.cpp
+++ 凯伦/凯伦/test1.cpp
@@ -1,40 +1,71 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
-long long n = 0;    // n 表示 01 串的长度
-long long t = 0;    // t 表示变换的次数
-int x = 1;          // 用于计算循环周期
-char s[10005];      // 存储 01 串的字符数组
+#define MIN(a, b) ((a) < (b) ? (a) : (b))  // 用于返回两个数中的较小值
+
+char S[1005], T[1005];   // 用于表示输入字符串的字符数组
+int dp[1005][1005];
 
 int main()
 {
-    scanf("%lld %lld", &n, &t);
-    scanf("%s", s);
-
-    // 计算 x 的最小值（这个值用于确定变换的周期），使得 x >= n
-    while (x < n)
-    {
-        // 通过左移操作找到大于或等于 n 的最小的 2 的幂次方值
-        x <<= 1;
-    }
-
-    // 减少不必要的重复计算
-    t = t % x;
-
-    // 进行 t 次操作
+    scanf("%s %s", S, T);
     int i = 0;
-    for (int i = 0; i < t; i++)
+    int j = 0;
+
+    // ls 和 lt 分别表示 S 和 T 的长度
+    int ls = strlen(S);
+    int lt = strlen(T);
+
+    // 初始化 dp 数组
+    for (i = 0; i <= ls; i++)
     {
-        int j = 0;
-        // 对当前字符和前一个字符进行异或操作
-        for (j = n - 1; j >= 1; j--)
+        for (j = 0; j <= lt; j++)
         {
-            // s[j] - '0' 表示将字符转换为数字（0 或 1）, + '0' 表示将结果转换回字符形式
-            s[j] = ((s[j] - '0') ^ (s[j - 1] - '0')) + '0';
+            // 将 dp 数组初始化为一个很大的数（0x3f3f3f3f）
+            dp[i][j] = 0x3f3f3f3f;
         }
     }
 
-    printf("%s\n", s);
+    // 当 T 为空字符串时，dp[i][0] 设置为0，因为不需要任何修改
+    for (i = 0; i <= ls; i++)
+    {
+        dp[i][0] = 0;
+    }
 
+    // 将字符串 S 和 T 的字符向后移动一位，并在开头添加一个空格
+    for (i = ls; i >= 1; i--)
+    {
+        S[i] = S[i - 1];
+    }
+    S[0] = ' ';
+
+    for (i = lt; i >= 1; i--)
+    {
+        T[i] = T[i - 1];
+    }
+    T[0] = ' ';
+
+    // 填充动态规划表
+    for (i = 1; i <= ls; i++)
+    {
+        for (j = 1; j <= lt; j++)
+        {
+            if (S[i] == T[j])
+            {
+                // 不需要修改 S[i]
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+            else
+            {
+                // 可以选择修改 S[i] 为 T[j]，则 dp[i][j] = dp[i-1][j-1] + 1
+                // 或者选择不匹配 T[j]，则 dp[i][j] = dp[i - 1][j]
+                // 取这两种情况的最小值
+                dp[i][j] = MIN(dp[i][j], MIN(dp[i - 1][j], dp[i - 1][j - 1] + 1));
+            }
+        }
+    }
+
+    printf("%d\n", dp[ls][lt]);
     return 0;
 }
