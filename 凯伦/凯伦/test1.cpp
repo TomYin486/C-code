2c55aa82193a48bfbd3bd0e012142b1dc2b0b339
jojo--1 <1468608544@qq.com>
2025-01-08 22:09:15 +0800

    C：数组 nums 包含从 0 到 n 的所有整数，但其中缺了一个。实现一个函数找出那个缺失的 整数且在 O(n) 时间内完成

--- 凯伦/凯伦/test1.cpp
+++ 凯伦/凯伦/test1.cpp
@@ -1,70 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAX_SIZE 100005   // 表示数组的最大大小
+int missingNumber(int* nums, int numsSize)
+{
+    int x = 0;
+    int i = 0;
+    // 计算 0 到 numsSize 的总和
+    // for (i = 0; i <= numsSize; i++) 
+    // {
+    //     x += i;
+    // }
+    x = numsSize * (numsSize + 1) / 2;    // 使用等差数列求和公式
+    for (i = 0; i < numsSize; i++)
+    {
+        x -= nums[i];
+    }
+    return x;
+}
 
 int main()
 {
-    // n，m 分别表示两个数组的长度
-    int n = 0;
-    int m = 0;
+    int nums[] = { 0,2,3,4,5,6 };
+    int numsSize = sizeof(nums) / sizeof(nums[0]);   // 计算数组的大小
+    int ret = missingNumber(nums, numsSize);
+    printf("%d\n", ret);
 
-    // a，b 用于表示输入的两个数组
-    int a[MAX_SIZE];
-    int b[MAX_SIZE];
-
-    // front1，front2 用于表示两个数组的当前处理位置（队列的前端）
-    int front1 = 0;
-    int front2 = 0;
-
-    // rear1，rear2 用于表示两个数组的末尾位置（队列的后端）
-    int rear1;
-    int rear2;
-
-    // sum 表示合并操作的次数
-    long long sum = 0;
-
-    scanf("%d %d", &n, &m);
-
-    rear1 = n;    // 表示 a 数组的末尾位置
-    rear2 = m;    // 表示 b 数组的末尾位置
-
-    int i = 0;
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    }
-    for (i = 0; i < m; i++)
-    {
-        scanf("%d", &b[i]);
-    }
-
-    // 通过合并操作使数组 a 和 b 变得完全相同
-    while (front1 < rear1 && front2 < rear2)
-    {
-        // 如果 a 和 b 当前处理的元素相等，则直接移动到下一个元素
-        if (a[front1] == b[front2])
-        {
-            front1++;
-            front2++;
-        }
-        // 如果 a 当前元素大于 b 当前元素，则将 b 当前元素与下一个元素合并，并增加合并次数
-        else if (a[front1] > b[front2])
-        {
-            a[front2 + 1] += b[front2];
-            front2++;
-            sum++;
-        }
-        // 如果 a 当前元素小于 b 当前元素，则将 a 当前元素与下一个元素合并，并增加合并次数
-        else
-        {
-            a[front1 + 1] += a[front1];
-            front1++;
-            sum++;
-        }
-    }
-
-    printf("%lld\n", sum);
     return 0;
 }
