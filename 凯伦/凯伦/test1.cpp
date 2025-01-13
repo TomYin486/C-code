d16fd833d242c4e72f50a22c7f97cab3f032eaa3
jojo--1 <1468608544@qq.com>
2025-01-13 23:04:20 +0800

    C：方法二（使用异或操作 ^），数组 nums 包含从 0 到 n 的所有整数，但其中缺了一个。 实现一个函数找出那个缺失的整数且在 O(n) 时间内完成

--- 凯伦/凯伦/test1.cpp
+++ 凯伦/凯伦/test1.cpp
@@ -1,50 +1,48 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-// 所有大于 2 的偶数都是合数，当 1 + 3 > 2 时，任何两个具有相同奇偶性的数相加都会得到一个合数
-// 现在需要找到一个奇数和一个偶数，让它们的和是一个合数
-// 可以验证在 n <= 4 时不存在这样的数对，但在 n = 5 ，存在 4 和 5，它们的和为 9，是合数
+// 使用 ^ 可防止数据太大而溢出（如计算 (n+1) * n / 2）
+// 异或操作的特点: 
+// 1.任何数和 0 异或，结果仍然是原来的数
+// 2.任何数和自身异或，结果是 0
+// 3.异或操作满足交换律和结合律
+
+int missingNumber(int* nums, int numsSize)
+{
+	int x = 0;       // x 用于表示异或结果
+	int n = numsSize;
+	int i = 0;
+
+	// 对 0 到 n 的所有整数进行异或操作，得到一个中间结果。这个结果相当于 0 到 n 的所有整数的异或和
+	for (i = 0; i <= n; i++)
+	{
+		x ^= i;
+	}
+
+	// 对数组中的每个元素进行异或操作
+	// 由于数组中的元素是 0 到 n 中缺失一个整数的集合，异或操作会将重复的数抵消掉，最终剩下的就是缺失的整数
+	for (i = 0; i < numsSize; i++)
+	{
+		x ^= nums[i];
+	}
+	return x;
+
+	// 也可以简化
+	/*int x = 0;
+	for (int i = 0; i < numsSize; i++)
+	{
+		x ^= nums[i];
+		x ^= i;
+	}
+	x ^= numsSize;
+	return x;*/
+}
 
 int main()
 {
-    int t = 0;
-    int n = 0;
-    int i = 0;
-    scanf("%d", &t);
-
-    while (t--)
-    {
-        scanf("%d", &n);
-
-        if (n < 5)
-        {
-            printf("-1\n");
-            continue;
-        }
-
-        // 输出所有偶数，除了 4
-        for (i = 2; i <= n; i += 2)
-        {
-            if (i != 4)
-            {
-                printf("%d ", i);
-            }
-        }
-
-        // 输出 4 和 5
-        printf("4 5 ");
-
-        // 输出所有奇数，除了 5
-        for (i = 1; i <= n; i += 2)
-        {
-            if (i != 5)
-            {
-                printf("%d ", i);
-            }
-        }
-
-        printf("\n");
-    }
-
-    return 0;
+	int nums[] = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+	int numsSize = sizeof(nums) / sizeof(nums[0]);   // 计算数组的大小
+	int ret = missingNumber(nums, numsSize);
+	printf("%d\n", ret);
+	return 0;
 }
