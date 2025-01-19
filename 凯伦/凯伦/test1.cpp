9496ed9adf00067d9b46c29aa352bd8ca2310234
jojo--1 <1468608544@qq.com>
2025-01-19 21:19:05 +0800

    C：（876）链表的中间结点，实现一个函数（使用快慢指针）对其给定的一个单链表找到单链表的中间节点。如果链表有偶数个节点，即有两个中间节点，需要返回第二个中间节点

--- 凯伦/凯伦/test1.cpp
+++ 凯伦/凯伦/test1.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 单链表
+// Definition for singly-linked list.
 struct ListNode {
     int val;
     struct ListNode* next;
@@ -10,100 +10,19 @@ struct ListNode {
 
 typedef struct ListNode ListNode;
 
-// 定义新链表，遍历原链表找不为 val 的节点，尾插在新链表中
-struct ListNode* removeElements(struct ListNode* head, int val)
-{
-    ListNode* newHead = NULL;
-    ListNode* newTail = NULL;
+struct ListNode* middleNode(struct ListNode* head) {
+    // slow 和 fast 初始时都指向链表的头节点 head
+    ListNode* slow = head;
+    ListNode* fast = head;
 
-    ListNode* pcur = head;
-
-    while (pcur != NULL)
+    // 注意两个循环条件不能写反了，不然会出问题
+    // 如果 fast 为 NULL，fast->next 会导致访问空指针
+    while (fast != NULL && fast->next != NULL)
     {
-        // 不是 val，尾插到新链表
-        if (pcur->val != val)
-        {
-            // 链表为空：插入进来的节点就是链表的头节点和尾节点
-            if (newHead == NULL)
-            {
-                newHead = newTail = pcur;
-            }
-            // 链表不为空：插入进来的节点就是链表的新的尾节点
-            else
-            {
-                // 让 newTail 变为新的尾节点
-                newTail->next = pcur;
-                newTail = newTail->next;
-            }
-        }
-        pcur = pcur->next;
+        // 让 slow 指针每次走一步，fast 指针每次走两步 
+        // 当 fast = NULL 或 fast->next = NULL 时，则 slow 指针刚好指向的就是中间的节点
+        slow = slow->next;
+        fast = fast->next->next;
     }
-    // 确保新链表的最后一个节点正确终止
-    if (newTail != NULL)
-    {
-        newTail->next = NULL;
-    }
-    // 返回新链表的头节点
-    return newHead;
-}
-
-// 打印链表
-void printList(ListNode* head)
-{
-    ListNode* pcur = head;
-    while (pcur != NULL)
-    {
-        printf("%d->", pcur->val);
-        pcur = pcur->next;
-    }
-    printf("NULL\n");
-}
-
-// 释放链表内存
-void freeList(ListNode* head)
-{
-    ListNode* pcur = head;
-    while (pcur != NULL)
-    {
-        ListNode* next = pcur->next;
-        free(pcur);
-        pcur = next;
-    }
-}
-
-int main()
-{
-    // 创建链表 [1, 2, 3]
-    ListNode* s1 = (ListNode*)malloc(sizeof(ListNode));
-    s1->val = 1;
-
-    ListNode* s2 = (ListNode*)malloc(sizeof(ListNode));
-    s2->val = 2;
-
-    ListNode* s3 = (ListNode*)malloc(sizeof(ListNode));
-    s3->val = 3;
-
-    s1->next = s2;
-    s2->next = s3;
-    s3->next = NULL;
-
-    // 打印原始链表
-    printf("Original list: ");
-    printList(s1);
-
-    int val = 2;
-
-    ListNode* newHead = removeElements(s1, val);
-
-    // 打印处理后的链表
-    printf("List after removing %d: ", val);
-    printList(newHead);
-
-    // 释放新链表的内存
-    freeList(newHead);
-
-    // 注意：不要释放原始链表的节点，因为它们可能已经被新链表使用
-    // 不能这么写 freeList(s1); 
-
-    return 0;
+    return slow;
 }
